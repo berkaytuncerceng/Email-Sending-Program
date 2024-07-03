98fda0e771a9d85d52fc7979f690cd98c521d7a3
Berkay Tuncer <berkay.beren11@gmail.com>
2024-07-03 19:00:27 +0300

    C# Console App  to Send Emails

---
+++ SendEmail/EmailCheck/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+class Program
+{
+    static void Main()
+    {
+        MailMessage eposta = new MailMessage();
+
+        void MailGonder()
+        {
+            try
+            {
+                eposta.From = new MailAddress("senderadress@outlook.com");
+                eposta.To.Add("receiveradress@gmail.com");
+                eposta.Subject = "the subject";
+                eposta.Body = "the whole message";
+
+                SmtpClient smtp = new SmtpClient
+                {
+                    Host = "smtp.office365.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    Credentials = new NetworkCredential("senderadress@outlook.com", "password"),
+                };
+
+                smtp.Send(eposta);
+                Console.WriteLine("The Mail has sent successfully");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        MailGonder();
+    }
+}
